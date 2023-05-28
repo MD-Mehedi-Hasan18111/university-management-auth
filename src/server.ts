f17f8ffd0063d85e3ee081c1c8b7e33f12a19114
MD Mehedi Hasan <md@bursement.com>
2023-05-29 01:18:50 +0600

    Configure eslint prettier

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Database Connected Successfully!");
+    await mongoose.connect(config.database_url as string)
+    console.log('Database Connected Successfully!')
     app.listen(config.port, () => {
       console.log(`App listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
-main();
+main()
